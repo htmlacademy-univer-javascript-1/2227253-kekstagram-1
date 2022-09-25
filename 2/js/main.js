9d7f2dabbb5d2c7864473574c88445a141675aa5
Keks <keks@htmlacademy.ru>
2022-09-25 18:17:24 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,12 +1,12 @@
-const getRandonIntegerInterval = (start, end) => {
-    if (start < 0 || end < 0) { return NaN; }
-    if (start > end) {[start, end] = [end, start]}
-    return Math.floor(Math.random() * (end - start + 1) ) + start;
-}
-// console.log(getRandonIntegerInterval(1, 1.5))
+const getRandonIntegerInterval = function (start, end) {
+  if (start < 0 || end < 0) { return NaN; }
+  if (start > end) {[start, end] = [end, start]; }
+  return Math.floor(Math.random() * (end - start + 1) ) + start;
+};
 
-const checkMaxStringLength = (str, maxLength) => {
-    return typeof(str) == 'string' && str.length >= maxLength;
-}
+getRandonIntegerInterval(1, 1.5);
 
-// console.log(checkMaxStringLength(123, 5))
+const checkMaxStringLength = (str, maxLength) => typeof(str) === 'string' && str.length >= maxLength;
+
+
+checkMaxStringLength(123, 5);
